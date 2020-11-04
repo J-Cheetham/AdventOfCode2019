15e8b7fe6341b88885580c80fb38d8164799455e
J_Cheetham <j20.jc@hotmail.com>
2020-11-04 10:31:52 +0000

    Advent Of Code Day 6 part 1 completed. Test input passes but doesn't work for the main puzzle input.

--- AdventOfCode6/AdventOfCode6/AdventOfCode6.cpp
+++ AdventOfCode6/AdventOfCode6/AdventOfCode6.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -58,6 +59,97 @@ void parseInput(vector<string>& orbitInput, vector<Orbit>& orbits)
     }
 }
 
+map<string, vector<string>> buildDiGraph(vector<Orbit>& orbits)
+{
+    map<string, vector<string>> diGraph;
+    for (Orbit& orbit : orbits)
+    {
+        map<string, vector<string>>::iterator it;
+        it = diGraph.find(orbit.Root);
+
+        //if the root is not in the map as a key
+        if (it == diGraph.end())
+        {
+            //add it to the map with the corresponding connection (its orbital)
+            vector<string> children;
+            children.push_back(orbit.Orbital);
+            diGraph.emplace(orbit.Root, children);
+        }
+        //the root is in the map, check the orbital is listed as a connection
+        else
+        {
+            //iterate through vector of connections to see if it's present
+            if (find(it->second.begin(), it->second.end(), orbit.Orbital) == it->second.end())
+            {
+                //add the orbital as it's not in the vector of connections
+                it->second.push_back(orbit.Orbital);
+            }
+        }
+
+        //redeclare the iterator, you may have inserted an element which would render it invalid...
+        it = diGraph.find(orbit.Orbital);
+
+        //check the orbital to make sure that's in the map as well
+        //if it's not, add it with it's connections
+        if (it == diGraph.end())
+        {
+            vector<string> children;
+            children.push_back(orbit.Root);
+            diGraph.emplace(orbit.Orbital, children);
+        }
+        //the orbital is in the map, make sure the root is there as a connection
+        else
+        {
+            //iterate through vector of connections to see if it's present
+            if (find(it->second.begin(), it->second.end(), orbit.Root) == it->second.end())
+            {
+                //add the orbital as it's not in the vector of connections
+                it->second.push_back(orbit.Root);
+            }
+        }
+    }
+    return diGraph;
+}
+
+vector<string> findShortestPath(map<string, vector<string>>& diGraph, string start, string end, vector<string> path, vector<string> shortest)
+{
+    path.push_back(start);
+
+    //cout << "Current Path: ";
+    //for (string str : path)
+    //{
+    //    cout << str << ", ";
+    //}
+    //cout << endl;
+
+    if (start == end)
+    {
+        return path;
+    }
+    vector<string> children = diGraph.find(start)->second;
+    for (string child : children)
+    {
+        //check child is not in the path already (avoids cycles)
+        if (find(path.begin(), path.end(), child) == path.end())
+        {
+            //check the current path is shorter than the shortest path found so far
+            if (shortest.empty() || path.empty() < shortest.size())
+            {
+                //recursive call to continue exploring that branch
+                vector<string> newPath = findShortestPath(diGraph, child, end, path, shortest);
+                
+                //a return path means the newPath must be shorter than the current path
+                if (!newPath.empty())
+                {
+                    shortest = newPath;
+                    cout << "Current shortest path length: " << shortest.size() << endl;
+                }
+            }
+        }
+    }
+    return shortest;
+}
+
 void createOrbitalMap(vector<Pair>& orbitalMap, vector<Orbit>& orbits)
 {
     //insert first element if map is empty
@@ -90,20 +182,44 @@ void createOrbitalMap(vector<Pair>& orbitalMap, vector<Orbit>& orbits)
 
 int main()
 {
-    //vector<string> orbitInput = { "COM)B", "B)C", "C)D", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L" };
+    //vector<string> orbitInput = { "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN", "K)SAN" };
     vector<string> orbitInput = loadFile();
     vector<Orbit> orbits;
     vector<Pair> orbitalMap;
 
     parseInput(orbitInput, orbits);
-    createOrbitalMap(orbitalMap, orbits);
+    map<string, vector<string>> properMap = buildDiGraph(orbits);
+
+    /*for (map<string, vector<string>>::iterator it = properMap.begin(); it != properMap.end(); it++)
+    {
+        cout << "Node: " << it->first << " has children: ";
+        for (string child : it->second)
+        {
+            cout << child << ", ";
+        }
+        cout << endl;
+    }*/
+
+    vector<string> path;
+    vector<string> shortest;
+    vector<string> finalPath = findShortestPath(properMap, "YOU", "SAN", path, shortest);
+
+    cout << "Shortest Path: ";
+    for (string s : finalPath)
+    {
+        cout << s << ", ";
+    }
+    cout << endl;
+    cout << "Number of steps of shortest path: " << finalPath.size() << endl;
+
+    //createOrbitalMap(orbitalMap, orbits);
 
     //calculate number of direct and indirect orbits
-    int totalOrbits = 0;
+    /*int totalOrbits = 0;
     for (vector<Pair>::iterator it = orbitalMap.begin(); it != orbitalMap.end(); it++)
     {
         cout << it->Name << " => " << it->OrbitCount << endl;
         totalOrbits += it->OrbitCount;
     }
-    cout << "Total Orbits: " << totalOrbits << endl;
+    cout << "Total Orbits: " << totalOrbits << endl;*/
 }
